b85a3a14acfe383fc73e41506f9b8aa92cb459fe
Bartosz Sepioło <bartosz.sepiolo@hotmail.com>
2023-09-09 20:10:31 +0200

    react form hook added

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,33 +1,55 @@
+"use client";
 import Link from "next/link";
 import Messages from "@/components/Messages";
-
+import { useForm, SubmitHandler } from "react-hook-form";
+type FormFields = {
+  email: string;
+  password: string;
+};
 export default function SignUp() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormFields>();
+  const onSubmit: SubmitHandler<FormFields> = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
       <form
         className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-        action="/auth/sign-up"
-        method="post"
+        onSubmit={handleSubmit(onSubmit)}
+        noValidate
       >
         <label className="text-md" htmlFor="email">
           Email
         </label>
         <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          name="email"
+          id="email"
+          type="email"
           placeholder="you@example.com"
-          required
+          aria-invalid={errors.email ? "true" : "false"}
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          {...register("email", { required: true })}
         />
+        {errors.email && <span role="alert">This field is required</span>}
+
         <label className="text-md" htmlFor="password">
           Password
         </label>
         <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          id="password"
           type="password"
-          name="password"
           placeholder="••••••••"
-          required
+          aria-invalid={errors.password ? "true" : "false"}
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          {...register("password", { required: true })}
         />
+        {errors.password && <span role="alert">This field is required</span>}
+
         <button className="bg-green-700 rounded px-4 py-2 text-white mb-2">
           Sign Up
         </button>
