ac22bfd0af028ea429d706cda4c847c41b9f7e28
Bartosz Sepioło <bartosz.sepiolo@hotmail.com>
2023-09-09 18:34:49 +0200

    views separated

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,7 +1,44 @@
+import Link from "next/link";
+import Messages from "@/components/Messages";
+
 export default function SignUp() {
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-      test asd
+      <form
+        className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
+        action="/auth/sign-up"
+        method="post"
+      >
+        <label className="text-md" htmlFor="email">
+          Email
+        </label>
+        <input
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          name="email"
+          placeholder="you@example.com"
+          required
+        />
+        <label className="text-md" htmlFor="password">
+          Password
+        </label>
+        <input
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          type="password"
+          name="password"
+          placeholder="••••••••"
+          required
+        />
+        <button className="bg-green-700 rounded px-4 py-2 text-white mb-2">
+          Sign Up
+        </button>
+        <Link
+          href="/"
+          className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
+        >
+          Sign in
+        </Link>
+        <Messages />
+      </form>
     </div>
   );
 }
