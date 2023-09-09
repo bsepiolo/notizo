5e9c0126bf5204cac0284e58c4d47db7053702f9
Bartosz Sepioło <bartosz.sepiolo@hotmail.com>
2023-09-09 18:15:25 +0200

    project init changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        btn: {
-          background: 'hsl(var(--btn-background))',
-          'background-hover': 'hsl(var(--btn-background-hover))',
-        },
+        white: "var(--white)",
+        black: "var(--black)",
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        "gray-100": "var(--gray-100)",
+        "gray-200": "var(--gray-200)",
+        blue: "var(--blue)",
+        accent: "var(--accent)",
       },
     },
   },
   plugins: [],
-}
+};
