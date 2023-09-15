d09d8c8d0a244081c358afc621a8c140f1622c4c
Bartosz Sepioło <bartosz.sepiolo@hotmail.com>
2023-09-15 21:39:38 +0200

    supabase fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,56 @@
+"use client";
 import Link from "next/link";
 import Messages from "@/components/Messages";
+import { SubmitHandler } from "react-hook-form";
 
+import Form from "@/components/Form";
+import FormControl from "@/components/FormControl";
+import TextBox from "@/components/TextBox";
+import Button from "@/components/Button";
+import FieldLabel from "@/components/FieldLabel";
+import { VALIDATION_RULES } from "@/constants/validation-rules";
+
+type FormFields = {
+  email: string;
+  password: string;
+};
 export default function Index() {
+  const onSubmit: SubmitHandler<FormFields> = async (formData) => {
+    console.log(formData);
+    await fetch("/auth/sign-in", {
+      method: "POST",
+      body: JSON.stringify(formData),
+    });
+  };
+
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-      <form
-        className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-        action="/auth/sign-in"
-        method="post"
-      >
-        <label className="text-md" htmlFor="email">
-          Email
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          name="email"
-          placeholder="you@example.com"
-          required
-        />
-        <label className="text-md" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          type="password"
-          name="password"
-          placeholder="••••••••"
-          required
-        />
-        <button className="bg-green-700 rounded px-4 py-2 text-white mb-2">
-          Sign In
-        </button>
-        <Link
-          href="/sign-up"
-          className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
-        >
-          Sign up
-        </Link>
+      <Form onSubmit={onSubmit}>
+        <FormControl rules={VALIDATION_RULES.email}>
+          <FieldLabel htmlFor="email">Email</FieldLabel>
+          <TextBox
+            id="email"
+            name="email"
+            type="email"
+            placeholder="you@example.com"
+          />
+        </FormControl>
+
+        <FormControl rules={VALIDATION_RULES.password}>
+          <FieldLabel htmlFor="password">Password</FieldLabel>
+          <TextBox
+            id="password"
+            name="password"
+            type="password"
+            placeholder="••••••••"
+          />
+        </FormControl>
+        <Button type="submit" variant="primary" className="mt-4">
+          Sign Up
+        </Button>
+
         <Messages />
-      </form>
+      </Form>
     </div>
   );
 }
